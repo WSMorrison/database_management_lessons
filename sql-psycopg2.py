d8a73a6d0442d5748163ef508f8c103fd8041154
WSMorrison <willimo@gmail.com>
2023-01-04 17:57:01 +0000

    Build a few different cursor executes.

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -4,6 +4,24 @@ connection = psycopg2.connect(database="chinook")
 
 cursor = connection.cursor()
 
+# cursor.execute('SELECT * FROM "Artist"')
+
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["The Rolling Stones"])
+
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [142])
+
+#cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Cher"])
+
 results = cursor.fetchall()
 
 # fecth one result, option
